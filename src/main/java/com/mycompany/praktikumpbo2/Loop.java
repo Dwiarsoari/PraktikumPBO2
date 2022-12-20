5c9d85ce45208144df70def9c835aa2cce471359
hp <hp@LAPTOP-78BK4H3P>
2022-12-20 18:02:29 +0700

    Pertemuan9

---
+++ src/main/java/com/mycompany/praktikumpbo2/Loop.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikumpbo2;
+
+/**
+ *
+ * @author hp
+ */
+public class Loop {
+  public static void main(String[] args) {
+  
+    // For
+    for(int i=0; i<10; i++) {
+      System.out.println("Looping... (" + i + ")");
+    }
+    System.out.println("Looping For selesai! \n");
+
+
+    // While
+    int j = 1;
+    while(j <= 100) {
+      System.out.println("Loading... (" + j + "%)");
+      j += 10;
+    }
+    System.out.println("Looping While selesai! \n");
+
+
+    // Do-While
+    int k = 10;
+    do {
+      System.out.println("Looping... (" + k + ")");
+      k--;
+    } while(k > 0);
+    System.out.println("Looping Do-While selesai! \n");
+  }
+}
---
+++ src/main/java/com/mycompany/praktikumpbo2/Variables.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikumpbo2;
+
+/**
+ *
+ * @author hp
+ */
+public class Variables {
+  public static void main(String[] args) {
+    String nama = "Dwi Arso Ari Saputra";
+    int usia = 21;
+    
+    System.out.println("Nama saya: " + nama);
+    System.out.println("Usia saya: " + usia + " tahun");
+  }
+}
